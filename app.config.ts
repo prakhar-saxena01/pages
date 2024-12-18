c6fb835ffa3cb2e0540acfa320b6a232abc5430d
prakhar-saxena01 <prakhar_saxena@outlook.com>
2024-12-18 16:15:36 +0530

    upload

---
+++ app.config.ts
@@ -0,0 +1,27 @@
+// https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
+export default defineAppConfig({
+  docus: {
+    title: 'My Notes',
+    description: 'The best place to start your documentation.',
+    url: 'https://prakhar.pro',
+    aside: {
+      level: 0,
+      collapsed: true,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: true
+    },
+    header: {
+      logo: false,
+      showLinkIcon: true,
+      exclude: [],
+      fluid: true
+    },
+    footer: {
+      credits: {text: 'Made by Prakhar Saxena', icon: 'IconDocus',},
+      fluid: true
+    },
+  }
+})
